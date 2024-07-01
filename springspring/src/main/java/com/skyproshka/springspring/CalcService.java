2783c71e0eb29a26695ed4bd79cbbaeb54f82e27
asdtt <asdttooppp@gmail.com>
2024-07-01 23:25:06 +0500

    Homework correct

--- springspring/src/main/java/com/skyproshka/springspring/CalcService.java
+++ springspring/src/main/java/com/skyproshka/springspring/CalcService.java
@@ -4,16 +4,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalcService {
-    public int getSum(int num1, int num2){
+    public static int getSum(int num1, int num2){
        return num1 + num2;
     }
-    public int getSub(int num1, int num2) {
+    public static int getSub(int num1, int num2) {
         return Integer.parseInt(num1 + " - " + num2 + " = " + (num1 + num2));
     }
-    public int getMult(int num1, int num2) {
+    public static int getMult(int num1, int num2) {
             return Integer.parseInt(num1 + " * " + num2 + " = " + (num1 + num2));
         }
-    public int getDiv(int num1, int num2) {
+    public static int getDiv(int num1, int num2) {
         return Integer.parseInt(num1 + " / " + num2 + " = " + (num1 + num2));
     }
 }
